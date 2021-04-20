49c2cd139043298a47c6f267132b0a9eaab7bdd9
petershen121 <59627435+petershen121@users.noreply.github.com>
2021-04-19 18:11:52 -0600

    Add files via upload

---
+++ Police_Office.js
@@ -0,0 +1,161 @@
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0115204,51.13455853 ]
+    },
+    "properties": {
+    "NAME":"AIRPORT OFFICE",
+    "STATION_TY":"COPS Stn.",
+    "ADDRESS":"2000 AIRPORT RD NE",
+    "INFO":"(403)735-1200"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0949898,51.09171981 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 3 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"4303 14 ST NW",
+    "INFO":"(403)428-6300"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0388,51.03060949 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 1 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"1010 26 AV SE",
+    "INFO":"(403)428-6100"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -113.9728807,51.09994994 ]
+    },
+    "properties": {
+    "NAME":"WESTWINDS CAMPUS",
+    "STATION_TY":"Headquarter",
+    "ADDRESS":"5111 47 ST NE",
+    "INFO":"(403)428-2200"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.1514266,51.03811185 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 2 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"4506 17 AV SW",
+    "INFO":"(403)428-6200"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -113.9445035,51.12446387 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 5 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"800 SADDLETOWNE CI NE",
+    "INFO":"(403)428-6500"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0361427,51.11206458 ]
+    },
+    "properties": {
+    "NAME":"CPS TRAFFIC SECTION",
+    "STATION_TY":"District Office",
+    "ADDRESS":"6528 11 ST NE",
+    "INFO":"(403)428-5500"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0679714,51.15942853 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 7 OFFICE (TRI-SERVICES)",
+    "STATION_TY":"District Office",
+    "ADDRESS":"11955 COUNTRY VILLAGE LI NE",
+    "INFO":"(403)428-6700"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0719993,50.99766179 ]
+    },
+    "properties": {
+    "NAME":"CHINOOK CENTRE MALL STATION",
+    "STATION_TY":"COPS Stn.",
+    "ADDRESS":"6455 MACLEOD TR S",
+    "INFO":"(403)428-5810"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0594053,50.90677619 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 8 OFFICE (TRI-SERVICES)",
+    "STATION_TY":"District Office",
+    "ADDRESS":"450 MIDPARK WY SE",
+    "INFO":"(403)428-6800"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -114.0685625,50.9785278 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 6 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"8325 BONAVENTURE DR SE",
+    "INFO":"(403)428-6600"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -113.9877144,51.06252888 ]
+    },
+    "properties": {
+    "NAME":"DISTRICT 4 OFFICE",
+    "STATION_TY":"District Office",
+    "ADDRESS":"3207 12 AV NE",
+    "INFO":"(403)428-6400"
+    }
+  }
+]
+}
